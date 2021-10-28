46ed7e70cd35f5aceb6f5f91d75ad8f9670a148a
Algimantas Backys <algimantas.backys@gmail.com>
2021-10-28 17:37:02 +0300

    package clean up

--- Package.swift
+++ Package.swift
@@ -21,24 +21,18 @@ let package = Package(
             targets: ["VGHtmlParser"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VGHtmlParser",
             path: "VGHtmlParser/",
-
             exclude: [
                 "Dependencies/hpple/README.markdown",
                 "Dependencies/hpple/LICENSE.txt"
             ],
             sources: ["Classes", "Dependencies"],
             publicHeadersPath: ".",
-
             cSettings: [
                 .headerSearchPath("Classes/**"),
                 .headerSearchPath("Dependencies/hpple/Classes/")
