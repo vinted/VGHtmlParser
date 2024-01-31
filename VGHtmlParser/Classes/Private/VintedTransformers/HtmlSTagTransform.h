686ef52a34ee839b8b54963f28b60bc96d1abbba
Lukas Plachtinas <lukas.plachtinas@vinted.com>
2024-01-31 22:11:18 +0200

    Add strikethrough support

---
+++ VGHtmlParser/Classes/Private/VintedTransformers/HtmlSTagTransform.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+#import "VGHtmlTagTransform.h"
+
+@interface HtmlSTagTransform : NSObject<VGHtmlTagTransform>
+
+@end
---
+++ VGHtmlParser/Classes/Private/VintedTransformers/HtmlSTagTransform.m
@@ -0,0 +1,24 @@
+#import <UIKit/UIKit.h>
+#import "HtmlSTagTransform.h"
+
+@implementation HtmlSTagTransform
+
+- (NSString *)tagName
+{
+    return @"s";
+}
+
+- (NSAttributedString *)transformAttributedString:(NSAttributedString *)attrString element:(TFHppleElement *)element
+{
+    if (!attrString.length) {
+        return attrString;
+    }
+    
+    NSMutableAttributedString *result = [attrString mutableCopy];
+    NSDictionary *attributes = @{@"TextAttributeStrikethrough":@""};
+    [result addAttributes:attributes range:NSMakeRange(0, result.length)];
+    return [result copy];
+}
+
+@end
+
