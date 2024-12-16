f9645780f8791a92d910fe0cb70cadabeab3b9a5
Aušra Balynė <ausra.balyne@vinted.com>
2024-12-16 16:20:44 +0200

    added TextAttributeHeadIndentEnabled attribute to the HtmlLiTagTransform so that <li> elements could be identified and edited within Bloomkit

--- VGHtmlParser/Classes/Private/VintedTransformers/HtmlLiTagTransform.m
+++ VGHtmlParser/Classes/Private/VintedTransformers/HtmlLiTagTransform.m
@@ -17,6 +17,10 @@
     NSMutableAttributedString *newAttrString = [[NSMutableAttributedString alloc] initWithString:@"  \u2022  "];
     [newAttrString appendAttributedString:[attrString mutableCopy]];
     [newAttrString appendAttributedString:[[NSAttributedString alloc] initWithString:@"\n"]];
+    
+    NSDictionary *attributes = @{@"TextAttributeHeadIndentEnabled":@true};
+    [newAttrString addAttributes:attributes range:NSMakeRange(0, newAttrString.length)];
+
     return [newAttrString copy];
 }
 
